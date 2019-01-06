12ee20c31d2d5221220bcfc1395c18c003804bca
Crash <hex22a@gmail.com>
2019-01-06 21:32:55 +0300

    Style main part

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,36 +1,83 @@
 import React, { Component } from 'react';
 import './Calculator.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffa994',
+      main: '#f67866',
+      dark: '#bf483b',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#8e99f3',
+      main: '#5c6bc0',
+      dark: '#26418f',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 class Calculator extends Component {
   render() {
     return (
-      <div className="calculator">
-        <h1 className="calculator__title">Rechner.</h1>
-        <form className="calculator__form">
-          <TextField
-            className="calculator__cidr-input"
-            variant="filled"
-            margin="normal"
-            label="CIDR"
-          />
-          <TextField
-            className="calculator__ip-input__half"
-            variant="filled"
-            margin="normal"
-            label="ip from"
-          />
-          <TextField
-            className="calculator__ip-input__half"
-            variant="filled"
-            margin="normal"
-            label="ip to"
-          />
-          <Button variant="contained" className="button">CLEAR</Button>
-          <Button variant="contained" className="button_primary">GO</Button>
-        </form>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="calculator">
+          <form className="calculator__form">
+            <h1 className="calculator__title">Rechner.</h1>
+            <TextField
+              className="calculator__input"
+              variant="filled"
+              margin="normal"
+              label="CIDR"
+            />
+            <div className="calculator__form-fieldset">
+              <div className="calculator__form-input__half">
+                <TextField
+                  className="calculator__input"
+                  variant="filled"
+                  margin="normal"
+                  label="IP from"
+                />
+              </div>
+              <div className="calculator__form-input__half">
+                <TextField
+                  className="calculator__input"
+                  variant="filled"
+                  margin="normal"
+                  label="IP to"
+                />
+              </div>
+            </div>
+            <div className="calculator__form-buttons">
+              <div className="calculator__form-input__half">
+                <Button
+                  className="button"
+                  variant="contained"
+                  color="secondary"
+                >
+                  Clear
+                </Button>
+              </div>
+              <div className="calculator__form-input__half">
+                <Button
+                  className="button"
+                  variant="contained"
+                  color="primary"
+                >
+                  Go
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,8 +6,8 @@ class Sidebar extends Component {
     return (
       <div className="sidebar">
         <h1 className="sidebar__title">Zhopa</h1>
-        <a className="sidebar__link">AddToBookMarks</a>
-        <a className="sidebar__link">Share</a>
+        <a className="sidebar__link" href="#1">AddToBookMarks</a>
+        <a className="sidebar__link" href="#1">Share</a>
         <a className="sidebar__link" href="https://github.com/crftd/rechner">GitHub</a>
       </div>
     );
