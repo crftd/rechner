de9bbc06c8e5ac41088bd45bd07f27d33b5f5be4
Crash <hex22a@gmail.com>
2019-01-16 01:46:47 +0300

    implement get IP method

--- src/core/CidrCalculator.js
+++ src/core/CidrCalculator.js
@@ -1,7 +1,12 @@
+import IP from './IP';
 import Mask from './Mask';
 import { OUT_OF_RANGE_ERROR_MESSAGE } from './constans';
 
 class CidrCalculator {
+  getIp(decimalIp) {
+    this.ip = new IP(decimalIp);
+  }
+
   getMask(bitAmount) {
     if (bitAmount < 8 || bitAmount > 30) {
       throw new Error(OUT_OF_RANGE_ERROR_MESSAGE);
--- src/core/__tests__/CidrCalculator.test.js
+++ src/core/__tests__/CidrCalculator.test.js
@@ -1,46 +1,92 @@
 import { OUT_OF_RANGE_ERROR_MESSAGE } from '../constans';
 
-class Mask {}
-
 describe('CidrCalculator', () => {
-  test('getMask', () => {
-    // Arrange
-    const mockMask = Mask;
-    jest.mock('../Mask', () => mockMask);
-    const CidrCalculator = require('../CidrCalculator').default;
-    const cidrCalculator = new CidrCalculator();
-    const expectedBitAmount = 24;
-    // Act
-    cidrCalculator.getMask(expectedBitAmount);
-    // Assert
-    expect(cidrCalculator.mask).toBeInstanceOf(Mask);
+  beforeEach(() => {
+    jest.resetModules();
   });
 
-  test('getMask - 7', () => {
-    // Arrange
-    const expectedBitAmount = 7;
-    const expectedErrorMessage = OUT_OF_RANGE_ERROR_MESSAGE;
-    const CidrCalculator = require('../CidrCalculator').default;
-    const cidrCalculator = new CidrCalculator();
-    // Act
-    function getMaskOutOfRange() {
-      cidrCalculator.getMask(expectedBitAmount);
-    }
-    // Assert
-    expect(getMaskOutOfRange).toThrow(expectedErrorMessage);
-  });
+  describe('getMask', () => {
+    class Mask {}
 
-  test('getMask - 31', () => {
-    // Arrange
-    const expectedBitAmount = 31;
-    const expectedErrorMessage = OUT_OF_RANGE_ERROR_MESSAGE;
-    const CidrCalculator = require('../CidrCalculator').default;
-    const cidrCalculator = new CidrCalculator();
-    // Act
-    function getMaskOutOfRange() {
+    test('getMask', () => {
+      // Arrange
+      const mockMask = Mask;
+      jest.mock('../Mask', () => mockMask);
+      const CidrCalculator = require('../CidrCalculator').default;
+      const cidrCalculator = new CidrCalculator();
+      const expectedBitAmount = 24;
+      // Act
       cidrCalculator.getMask(expectedBitAmount);
-    }
-    // Assert
-    expect(getMaskOutOfRange).toThrow(expectedErrorMessage);
+      // Assert
+      expect(cidrCalculator.mask).toBeInstanceOf(Mask);
+    });
+
+    test('getMask - 7', () => {
+      // Arrange
+      const expectedBitAmount = 7;
+      const expectedErrorMessage = OUT_OF_RANGE_ERROR_MESSAGE;
+      const CidrCalculator = require('../CidrCalculator').default;
+      const cidrCalculator = new CidrCalculator();
+      // Act
+      function getMaskOutOfRange() {
+        cidrCalculator.getMask(expectedBitAmount);
+      }
+      // Assert
+      expect(getMaskOutOfRange).toThrow(expectedErrorMessage);
+    });
+
+    test('getMask - 31', () => {
+      // Arrange
+      const expectedBitAmount = 31;
+      const expectedErrorMessage = OUT_OF_RANGE_ERROR_MESSAGE;
+      const CidrCalculator = require('../CidrCalculator').default;
+      const cidrCalculator = new CidrCalculator();
+      // Act
+      function getMaskOutOfRange() {
+        cidrCalculator.getMask(expectedBitAmount);
+      }
+      // Assert
+      expect(getMaskOutOfRange).toThrow(expectedErrorMessage);
+    });
+  });
+  describe('getIp', () => {
+    test('getIp - happy path', () => {
+      // Arrange
+      class IP {}
+      const expectedDecimalIp = '192.168.0.5';
+      const mockIp = IP;
+      jest.mock('../IP', () => mockIp);
+      const CidrCalculator = require('../CidrCalculator').default;
+      const cidrCalculator = new CidrCalculator();
+
+      // Act
+      cidrCalculator.getIp(expectedDecimalIp);
+
+      // Assert
+      expect(cidrCalculator.ip).toBeInstanceOf(IP);
+    });
+
+    test('getIp - out of range', () => {
+      // Arrange
+      class IP {
+        constructor() {
+          throw new Error(expectedErrorMessage);
+        }
+      }
+      const expectedDecimalIp = '300.168.0.5';
+      const expectedErrorMessage = OUT_OF_RANGE_ERROR_MESSAGE;
+      const mockIp = IP;
+      jest.mock('../IP', () => mockIp);
+      const CidrCalculator = require('../CidrCalculator').default;
+      const cidrCalculator = new CidrCalculator();
+
+      // Act
+      function getIpOutOfRange() {
+        cidrCalculator.getIp(expectedDecimalIp);
+      }
+
+      // Assert
+      expect(getIpOutOfRange).toThrow(expectedErrorMessage);
+    });
   });
 });
